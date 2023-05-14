37f023d2bda2d275c8bc8594d32a0a4a87278599
Wrapper <ajit.rai@gmail.com>
2023-05-13 23:51:22 -0400

    Create providers.tf

---
+++ private-aks-setup/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.15.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9"
+    }
+  }
+}
