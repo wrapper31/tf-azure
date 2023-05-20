9521b7cdf0dd5be467c5fa2f87f9362dd513bd1b
Wrapper <ajit.rai@gmail.com>
2023-05-20 18:10:50 -0400

    Create variables.tf

---
+++ terraform-azure-test/variables.tf
@@ -0,0 +1,5 @@
+variable "developer_name" {
+  type        = string
+  description = "Name of the developer using the test module, should be set from environment variable"
+  default     = "test"
+}
