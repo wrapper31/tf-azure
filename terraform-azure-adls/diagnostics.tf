71aa7f48bc75fcbfdf8a6e2bfb145b44c790eaf0
Wrapper <ajit.rai@gmail.com>
2023-05-21 00:08:36 -0400

    Create diagnostics.tf

---
+++ terraform-azure-adls/diagnostics.tf
@@ -0,0 +1,37 @@
+resource "azurerm_monitor_diagnostic_setting" "this" {
+  count = var.enable_diagnostics ? 1 : 0
+
+  name                       = lower(format("%s-%s", azurerm_storage_account.this.name, "diag"))
+  target_resource_id         = "${azurerm_storage_account.this.id}/blobServices/default/"
+  log_analytics_workspace_id = var.log_analytics_resource_id
+
+  dynamic "enabled_log" {
+    for_each = var.diagnostic_logs
+
+    content {
+      category = enabled_log.value.category
+
+      retention_policy {
+        enabled = enabled_log.value.retention_enabled
+        days    = enabled_log.value.retention_days
+      }
+    }
+  }
+
+  dynamic "metric" {
+    for_each = var.diagnostic_metrics
+
+    content {
+      category = metric.value.category
+      enabled  = metric.value.enabled
+      retention_policy {
+        enabled = metric.value.retention_enabled
+        days    = metric.value.retention_days
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [log_analytics_destination_type, metric]
+  }
+}
